81eeb72901de704ee8cd7e383a7ad318053a5b45
Hypersand <on8214@gmail.com>
2023-02-28 13:19:08 +0900

    fix: ArrayList를 이용한 풀이

--- src/main/java/Lecture59/Problem59_1.java
+++ src/main/java/Lecture59/Problem59_1.java
@@ -7,10 +7,12 @@ public class Problem59_1 {
         Scanner sc = new Scanner(System.in);
 
         int num = 0;
-        int[] arr = new int[1000000];
+//        int[] arr = new int[1000000];
+//        Arrays.fill(arr,-1);
         int k = 0;
+        List<Integer> list = new ArrayList<>();
 
-        Arrays.fill(arr,-1);
+
 
         while (true) {
             System.out.print("숫자를 입력해주세요(-1 : 종료) : ");
@@ -18,15 +20,20 @@ public class Problem59_1 {
             if (num == -1) {
                 break;
             }
-            arr[k] = num;
+//            arr[k] = num;
+            list.add(num);
             k++;
         }
 
-        Arrays.sort(arr);
+//        Arrays.sort(arr);
+        Collections.sort(list);
         System.out.println("입력을 종료합니다.");
         System.out.print("입력한 숫자(오름차순) : ");
-        for (int i = arr.length - k; i < arr.length; i++) {
-            System.out.print(arr[i]+ " ");
+//        for (int i = arr.length - k; i < arr.length; i++) {
+//            System.out.print(arr[i]+ " ");
+//        }
+        for (Integer ans : list) {
+            System.out.print(ans + " ");
         }
         System.out.println();
         System.out.println("프로그램을 종료합니다.");
