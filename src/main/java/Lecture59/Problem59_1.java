73df7b0787d61a1ad750ca6cc8d3e12fa7c5eb1a
Hypersand <on8214@gmail.com>
2023-02-28 13:38:00 +0900

    fix: 스트림 연산자 추가

--- src/main/java/Lecture59/Problem59_1.java
+++ src/main/java/Lecture59/Problem59_1.java
@@ -1,14 +1,14 @@
 package Lecture59;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem59_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
 
         int num = 0;
-//        int[] arr = new int[1000000];
-//        Arrays.fill(arr,-1);
         int k = 0;
         List<Integer> list = new ArrayList<>();
 
@@ -20,22 +20,21 @@ public class Problem59_1 {
             if (num == -1) {
                 break;
             }
-//            arr[k] = num;
+
             list.add(num);
             k++;
         }
 
-//        Arrays.sort(arr);
         Collections.sort(list);
         System.out.println("입력을 종료합니다.");
         System.out.print("입력한 숫자(오름차순) : ");
-//        for (int i = arr.length - k; i < arr.length; i++) {
-//            System.out.print(arr[i]+ " ");
-//        }
-        for (Integer ans : list) {
-            System.out.print(ans + " ");
-        }
-        System.out.println();
+
+        String str = list.stream()
+                        .sorted()
+                        .map(e -> "" + e)
+                        .collect(Collectors.joining(", "));
+
+        System.out.println(str);
         System.out.println("프로그램을 종료합니다.");
     }
 }
