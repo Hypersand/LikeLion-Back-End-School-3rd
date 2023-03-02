1e32c91bb78f43060766d6a183c56fff06614024
Hypersand <on8214@gmail.com>
2023-03-02 14:03:26 +0900

    feat: Entity 분리

---
+++ src/main/java/LifeQuotesProject/entity/WiseSaying.java
@@ -0,0 +1,25 @@
+package LifeQuotesProject.entity;
+
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
