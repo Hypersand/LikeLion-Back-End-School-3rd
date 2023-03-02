ce45f02f066a796ed1debea2fb60e97c1d0105ed
Hypersand <on8214@gmail.com>
2023-03-02 13:37:21 +0900

    fix: 명언 앱 프로젝트 10단계

--- src/main/java/LifeQuotesProject/LifeQuotesProject.java
+++ src/main/java/LifeQuotesProject/LifeQuotesProject.java
@@ -1,15 +1,25 @@
 package LifeQuotesProject;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class LifeQuotesProject {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         Map<Integer, 명언및작가> map = new HashMap<>();
+        String path = "/Users/bigsand/IdeaProjects/LikeLion/src/main/java/data.json";
+        JSONArray jsonArray = new JSONArray();
+        JSONObject json;
+        FileWriter fw = new FileWriter(path);
         System.out.println("== 명언 앱 ==");
         int count = 0;
+        int count2 = 0;
 
         while(true) {
             System.out.print("명령) ");
@@ -23,7 +33,12 @@ public class LifeQuotesProject {
                 System.out.print("작가 : ");
                 String 작가 = sc.nextLine();
                 count++;
-                map.put(count, new 명언및작가(명언, 작가));
+                map.put(count, new 명언및작가(count,명언, 작가));
+                json = new JSONObject();
+                json.put("id", count);
+                json.put("content", 명언);
+                json.put("author", 작가);
+                jsonArray.add(json);
                 System.out.println(count+"번 명언이 등록되었습니다.");
             }
             if (입력.equals("목록")) {
@@ -44,7 +59,9 @@ public class LifeQuotesProject {
                 }
                 else {
                     map.remove(숫자);
+                    jsonArray.remove(숫자 - 1);
                     System.out.println(숫자 + "번 명언이 삭제되었습니다.");
+                    count2++;
                 }
             }
 
@@ -57,17 +74,32 @@ public class LifeQuotesProject {
                 System.out.println("작가(기존) : " + map.get(숫자).작가);
                 System.out.print("작가 : ");
                 String 작가 = sc.nextLine();
-                map.put(숫자, new 명언및작가(명언, 작가));
+                map.put(숫자, new 명언및작가(숫자,명언, 작가));
+                jsonArray.remove(숫자-(1+count2));
+                JSONObject newJson = new JSONObject();
+                newJson.put("id", 숫자);
+                newJson.put("content", 명언);
+                newJson.put("author", 작가);
+                jsonArray.add(newJson);
             }
+
+            if (입력.equals("빌드")) {
+                System.out.println("data.json 파일의 내용이 갱신되었습니다.");
+                fw.write(jsonArray.toJSONString());
+                fw.close();
+            }
+
         }
 
     }
 
     public static class 명언및작가 {
+        int 인덱스;
         String 명언;
         String 작가;
 
-        public 명언및작가(String 명언, String 작가) {
+        public 명언및작가(int 인덱스, String 명언, String 작가) {
+            this.인덱스 = 인덱스;
             this.명언 = 명언;
             this.작가 = 작가;
         }
