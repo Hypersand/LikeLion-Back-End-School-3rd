5a59251e56070b2db69fdedc5e41ed311b0df726
Hypersand <on8214@gmail.com>
2023-03-02 09:27:16 +0900

    fix: 명언 앱 프로젝트 5단계

--- src/main/java/LifeQuotesProject/LifeQuotesProject.java
+++ src/main/java/LifeQuotesProject/LifeQuotesProject.java
@@ -1,10 +1,13 @@
 package LifeQuotesProject;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LifeQuotesProject {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Map<Integer, 명언및작가> map = new HashMap<>();
         System.out.println("== 명언 앱 ==");
         int count = 0;
 
@@ -20,9 +23,26 @@ public class LifeQuotesProject {
                 System.out.print("작가 : ");
                 String 작가 = sc.nextLine();
                 count++;
+                map.put(count, new 명언및작가(명언, 작가));
                 System.out.println(count+"번 명언이 등록되었습니다.");
+            }
+            if (입력.equals("목록")) {
+                for (int i = count; i > 0; i--) {
+                    System.out.println(i + " / " + map.get(count).작가 + " / " + map.get(count).명언);
+
+                }
             }
         }
 
+    }
+
+    public static class 명언및작가 {
+        String 명언;
+        String 작가;
+
+        public 명언및작가(String 명언, String 작가) {
+            this.명언 = 명언;
+            this.작가 = 작가;
+        }
     }
 }
